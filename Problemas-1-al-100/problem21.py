a3b5042b2d4a6a336f7cd30521c8f6848a8aa61e
Stalin-Cordoba <wisquiquikiri13@gmail.com>
2026-06-08 15:55:19 -0600

    Comentarios adicionales + Se me olvidó corregir los valores para 'p' y 'q'

--- Problemas-1-al-100/problem21.py
+++ Problemas-1-al-100/problem21.py
@@ -14,19 +14,21 @@ def main():
     
     amigosSuma = 0
 
+    # Se calcula los totales para los números del 1 al 9999
     sumaTotales = []
     
     for i in range(1, 10000, 1):
 
         sumaTotales.append(sumaDivisores(i))
 
-    p = 220
-    q = 284
+    p = 1
+    q = p + 1
 
     while p < 10000:
 
         while q < 10000:
 
+            # Se verifica la condición
             if sumaTotales[p - 1] == q and sumaTotales[q - 1] == p:
 
                 amigosSuma += p
